49b2a8d28b123bdaa7b970326be1167a10f67209
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SecuritySystemExample.Win/WinApplication.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using DevExpress.ExpressApp.Win;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Xpo;
+
+namespace SecuritySystemExample.Win {
+    public partial class SecuritySystemExampleWindowsFormsApplication : WinApplication {
+        public SecuritySystemExampleWindowsFormsApplication() {
+            InitializeComponent();
+            DelayedViewItemsInitialization = true;
+        }
+
+        protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args) {
+            args.ObjectSpaceProvider = new XPObjectSpaceProvider(args.ConnectionString, args.Connection);
+        }
+        private void SecuritySystemExampleWindowsFormsApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+        private void SecuritySystemExampleWindowsFormsApplication_CustomizeLanguagesList(object sender, CustomizeLanguagesListEventArgs e) {
+            string userLanguageName = System.Threading.Thread.CurrentThread.CurrentUICulture.Name;
+            if (userLanguageName != "en-US" && e.Languages.IndexOf(userLanguageName) == -1) {
+                e.Languages.Add(userLanguageName);
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to: Implement client-side security in the UI-level mode
+
+
+<p>The complete description is available in the <a href="http://documentation.devexpress.com/#xaf/CustomDocument3436"><u>Client-Side Security - UI-Level Mode</u></a> topic.</p>
+
+<br/>
+
+
